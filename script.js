78792d773925f3157a41650d04c91fc0f9dfe890
Dammy Knight <olaoluwaayomide10@gmail.com>
2023-08-18 14:34:36 +0100

    geolocating challenge 1

--- script.js
+++ script.js
@@ -68,7 +68,28 @@ const getCountryDatas = function (country) {
       return fetch(`https://restcountries.com/v3.1/alpha/${neigbor}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neigbor'));
+    .then(data => renderCountry(data, 'neigbor'))
+    .catch(error => console.log(error));
 };
 
-getCountryDatas('greece');
+btn.addEventListener('click', function () {
+  getCountryDatas('greece');
+});
+
+//challenges 1
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are based in ${data.city} in ${data.country}`);
+    })
+    .catch(err => console.log(`${err.message}`));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
