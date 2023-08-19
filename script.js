2498368a4cc6f0f6b000877b3db9c7f9fad003bd
Dammy Knight <olaoluwaayomide10@gmail.com>
2023-08-19 14:20:15 +0100

    returning values from async, running promises in parallel

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ createImage('img/img-1.jpg')
   })
   .catch(err => console.error(err));
 
-//async / await promise
+async / (await promise);
 const whereAreWe = async function (country) {
   try {
     const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
@@ -187,10 +187,48 @@ const whereAreWe = async function (country) {
     const data = await res.json();
     console.log(data);
     renderCountry(data[0]);
+    return `You are in ${data[0].capital}`;
   } catch (err) {
     console.error(err);
     renderError(`Something went wrong ${err.message}`);
+
+    //reject promise returned from async function
+    throw err;
   }
 };
 
 whereAreWe('france');
+const country = whereAreWe('france').then(country => console.log(country));
+console.log(country);
+
+// (async function () {
+//     try {
+//         const city = await whereAreWe()
+//     }
+// })()
+
+// Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
